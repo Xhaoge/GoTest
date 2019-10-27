3d8902ab24e8ed3d691c331e6fdf8fde98bafd2d
nima <1026737316@qq.com>
2019-10-27 18:42:32 +0800

    add oop

--- src/goBase/goOop.go
+++ src/goBase/goOop.go
@@ -8,6 +8,12 @@ type Xhaoge struct{
 	age int
 	work string
 	feature string
+	xbase
+}
+
+type xbase struct{
+	high int
+	des string
 }
 
 func (x *Xhaoge) Yutu() {
@@ -17,12 +23,29 @@ func (x *Xhaoge) Yutu() {
 	}	
 }
 
+type haoge struct{
+	xhaoges []Xhaoge
+}
+
+func (w *haoge)print_haoge() {
+	fmt.Println("this is print_haoge")
+	for _,x := range w.xhaoges {
+		fmt.Printf("struct:%v,  type:%T, name:%s,work:%s,des:%d\n",x,x,x.name,x.work,x.high)
+	}
+}
+
 
 func main(){
 	fmt.Println("test OOP")
-
-	var xx Xhaoge
-	xx = Xhaoge{name:"xhaoge",age:13,work:"yuetu",feature:"充满希望"}
+	des := xbase{171,"很帅"}
+	xx := Xhaoge{name:"xhaoge",age:13,work:"yuetu",feature:"充满希望",xbase:des}
 	xx.Yutu()
 	fmt.Printf("my name:%s, age:%d, work:%s, in the feature:%s\n",xx.name,xx.age,xx.work,xx.feature)
+	xxx :=Xhaoge{name:"xhao",age:17,work:"yuetu",feature:"财富自由",xbase:des}
+
+	w := haoge{
+		xhaoges: []Xhaoge{xx,xxx},
+	}
+	w.print_haoge()
+
 }
