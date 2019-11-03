af00438ac0317fd3ba8cb0d3bb5a774fafeb8650
nima <1026737316@qq.com>
2019-11-03 16:32:04 +0800

    add login

--- src/goWeb/myblog/controllers/login_controller.go
+++ src/goWeb/myblog/controllers/login_controller.go
@@ -14,3 +14,20 @@ type LoginController struct {
 func (this *LoginController) Get() {
 	this.TplName = "login.html"
 }
+
+func (this *LoginController) Post(){
+	username := this.GetString("username")
+	password := this.GetString("password")
+	fmt.Println("username:",username,"password:",password)
+
+	id := models.QueryUserWithParam(username,password)
+	fmt.Println("log 时登陆的账号id为：",id)
+	if id > 0{
+		this.Data["json"] = map[string]interface{}{"code":1,"message":"登陆成功"}
+	}else{
+		this.Data["json"] = map[string]interface{}{"code":0,"message":"登陆失败"}
+	}
+	this.ServeJSON()
+}
+
+
