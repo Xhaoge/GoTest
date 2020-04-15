d44d40350a582db7f4925690c6dafc8fa2985920
xiaohaogge <1026737316@qq.com>
2020-04-15 22:52:21 +0800

    add  grpc

--- goProgram/grpc-proto/client.go
+++ goProgram/grpc-proto/client.go
@@ -6,10 +6,10 @@ import (
 
 	"google.golang.org/grpc"
 
-	pb "github.com/EDDYCJY/go-grpc-example/proto"
+	pb "godie/goProgram/grpc-proto/protos"
 )
 
-const PORT = "9001"
+const PORT = "9091"
 
 func main() {
 	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
@@ -20,26 +20,24 @@ func main() {
 
 	client := pb.NewSHServiceClient(conn)
 	resp, err := client.Search(context.Background(), &pb.YuetuSearchRequest{
-		BaseRequest: &SimpleRequest{
+		BaseRequest: &pb.SimpleRequest{
 			Cid: "tongchengyt",
-			Trace_id: "2354435-jgidg",
+			TraceId: "2354435-jgidg",
 		},
-		Trip:&Trip{
+		Trip:&pb.Trip{
 			DepartureCode:"SIN",
 			ArrivalCode:"SYD",
 			DepartureDate:"2020-07-20T16:00:00Z",
 		},
-		Cabin:"E",
-		AduleNum:1,
+		AdultNum:1,
 		ChildNum: 0,
 		InfantNum:0,
 		BypassCache:false,
 		GodPerspective:false,
-		TargetProviders:[],
 	})
 	if err != nil {
 		log.Fatalf("client.Search err: %v", err)
 	}
 
-	log.Printf("resp: %s", resp.GetResponse())
+	log.Printf("resp: %s", resp)
 }
