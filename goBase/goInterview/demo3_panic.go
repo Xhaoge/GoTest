a1e41c8f2494dda063aa7b74020812266d5d35fe
xxx <1026737316@qq.com>
2020-05-13 14:10:53 +0800

    add interview

---
+++ goBase/goInterview/demo3_panic.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// func main() {
+//     go func() {
+//     	proc()
+//         // 1 在这里需要你写算法
+//         // 2 要求每秒钟调用一次proc函数
+//         // 3 要求程序不能退出
+//     }()
+
+//     select {}
+// }
+
+// func proc() {
+//     panic("ok")
+// }
+
+func proc() {
+	panic("ok")
+}
+
+func goproc(i int) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("%d panic err：%s\n", i, err)
+		}
+	}()
+	proc()
+}
+
+func main() {
+	i := 0
+	go func() {
+		for {
+			ticker := time.NewTicker(time.Second * 1)
+			defer ticker.Stop()
+			select {
+			case <-ticker.C
+				i++
+				go goproc(i)
+			}
+			
+		}
+	}()
+	select {}
+}
